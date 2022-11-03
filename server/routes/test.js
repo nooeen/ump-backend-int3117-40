1aff2ca88badfdf5d7e7f7a9eb0264ee5d7380c2
Duy Ngoc Nguyen <duyngoc.nguyen2001@gmail.com>
2022-11-03 13:27:11 +0700

    Update test.js

--- server/routes/test.js
+++ server/routes/test.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", function (req, res) {
-  res.send("Welcome to UMP's API Server!");
+  res.send("Welcome to UMP-INT3117-40's API Server!");
 });
 
 module.exports = router;
